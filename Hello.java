1aee577bd6c7bfb5ec02bb67e5b1da9c66a3f61e
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-09-29 10:43:07 +0000

    first_commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+    System.out.println(800-300 + "G");
+    System.out.println("残りHPは"+ (30+40) + "です。");
+    System.out.println(400*3*1.09);
+    System.out.println(15/3);
+    System.out.println(10/2+3);
+    System.out.println(10/(2+3));
+  }
+}
---
+++ game/Gold.java
@@ -0,0 +1,11 @@
+public class Gold{
+  public static void main(String[] args){
+    System.out.println("名前：勇者");
+    System.out.println("体力：50");
+    System.out.println("所持金：1000G");
+    System.out.println("250G拾った");
+    System.out.println(1000+250 + "G");
+    System.out.println("200G落とした");
+    System.out.println("あなたの所持金は" + (1250-200) + "G)です");
+  }
+}
